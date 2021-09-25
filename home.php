9293e6096a45d8a1a80e1b2da5b6dacdd730e477
ZachRochette <zachrochette@hotmail.com>
2021-09-25 05:09:18 -0600

    commit

--- home.php
+++ home.php
@@ -27,9 +27,13 @@
             <h2 id="upgrades">Delorean Upgrades</h2>
             <div class="upgrades">
                 <img src="/phpmotors/images/upgrades/flux-cap.png" alt="Flux Capacitor image">
+                <p>Flux Capacitor</p>
                 <img src="/phpmotors/images/upgrades/flame.jpg" alt="Flame Decals image">
+                <p>Flame Decals</p>
                 <img src="/phpmotors/images/upgrades/bumper_sticker.jpg" alt="Bumper Stickers image">
+                <p>Bumper Stickers</p>
                 <img src="/phpmotors/images/upgrades/hub-cap.jpg" alt="Hub Caps image">
+                <p>Hub Caps</p>
             </div>
             <h2 id="reviews">DMC Delorean Reviews</h2>
         </main>
