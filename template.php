d8ffb1c6a5ed5bb6e799a318127a9be02a98deda
ZachRochette <zachrochette@hotmail.com>
2021-09-25 03:29:06 -0600

    commit

--- template.php
+++ template.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>PHP Motors</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="/phpmotors/css/style.css" type="text/css" rel="stylesheet" media="screen">
 </head>
 
 <body>
