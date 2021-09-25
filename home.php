ac27abd90d7ab55c25151d858c4dcf05e1052e86
ZachRochette <zachrochette@hotmail.com>
2021-09-25 03:09:33 -0600

    commit

--- home.php
+++ home.php
@@ -11,29 +11,18 @@
 <body>
     <div id="wrapper">
         <header>
-            <div id="top-header">
-                <img src="images/site/logo.png" alt="PHP Motors logo" id="logo">
-                <a href="/accounts?action=login-page" title="Login or Register with PHP Motors" id="acc">My Account</a>
-            </div>
+
         </header>
         <nav>
             <ul>
-                <li> <a href="/phpmotors/" title="PHP Motors home page">Home</a></li>
-                <li> <a href="#" title="classic car page">Classic</a></li>
-                <li> <a href="#" title="sports cars">Sports</a></li>
-                <li> <a href="#" title="sports utility vehicles">SUV</a></li>
-                <li> <a href="#" title="trucks">Trucks</a></li>
-                <li> <a href="#" title="used cars">Used</a></li>
-            </ul>
+
         </nav>
         <main>
             <h1>Content Title Here</h1>
         </main>
         <hr>
         <footer>
-            <p>&copy; PHP Motors, All rights reserved.</p>
-            <p>Images are belieaaved to be "Fair Use". Notify the author if not and they will be removed.</p>
-            <p>Last Updated: <?php echo date('j F, Y', getlastmod()); ?></p>
+
         </footer>
     </div>
 </body>
---
+++ snippets/nav.php
@@ -0,0 +1,7 @@
+<li> <a href="/phpmotors/" title="PHP Motors home page">Home</a></li>
+<li> <a href="#" title="classic car page">Classic</a></li>
+<li> <a href="#" title="sports cars">Sports</a></li>
+<li> <a href="#" title="sports utility vehicles">SUV</a></li>
+<li> <a href="#" title="trucks">Trucks</a></li>
+<li> <a href="#" title="used cars">Used</a></li>
+</ul>
