0718cc20062370ec12dabe1beac2e0344b98762f
ZachRochette <zachrochette@hotmail.com>
2021-10-01 21:46:37 -0600

    commit

--- library/connections.php
+++ library/connections.php
@@ -2,23 +2,25 @@
 /*
 Proxy connection to the phpmotors databse
 */
-require 'library/connections.php';
+
 
 function phpmotorsConnect()
 {
     $server = 'mysql';
     $dbname = 'phpmotors';
     $username = 'iClient';
-    $password = 'Fb9*/bxYYcvjOQX9';
+    $password = 'RSf[rdh4Yf5KyFPe';
     $dsn = "mysql:host=$server;dbname=$dbname";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
     // Create the actual connection object and assign it to a variable
     try {
         $link = new PDO($dsn, $username, $password, $options);
-        return $link;
+        if (is_object($link)) {
+            echo "it worked!";
+        }
     } catch (PDOException $e) {
-        header('Location: /phpmotors/view/500.php');
-        exit;
+        echo "it didn't work: " . $e->getMessage();
     }
 }
+phpmotorsConnect();
