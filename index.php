89fa36b910cb619dc957773d49d65eaf343065e6
ZachRochette <zachrochette@hotmail.com>
2021-10-07 15:18:39 -0600

    commit

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 // This is the main controller 
 
+// Get the database connection file
+require_once 'library/connections.php';
+// Get the PHP Motors model for use as needed
+require_once 'model/main-model.php';
+
+// Get the array of classifications
+$classifications = getClassifications();
+var_dump($classifications);
+exit;
+
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_POST, 'action');
