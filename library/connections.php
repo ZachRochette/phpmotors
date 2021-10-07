3e45289ef4aa1c1992d2a8102818134b7ae6718f
ZachRochette <zachrochette@hotmail.com>
2021-10-07 15:32:12 -0600

    commit

--- library/connections.php
+++ library/connections.php
@@ -16,10 +16,9 @@ function phpmotorsConnect()
     // Create the actual connection object and assign it to a variable
     try {
         $link = new PDO($dsn, $username, $password, $options);
-        return $link
+        return $link;
     } catch (PDOException $e) {
         header('Location: /phpmotors/view/500.php');
         exit;
     }
 }
-
