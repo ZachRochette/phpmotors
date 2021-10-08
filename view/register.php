02b81893cdd8d972855af3282c8fe8af42aa84d9
ZachRochette <zachrochette@hotmail.com>
2021-10-07 19:29:52 -0600

    commit

---
+++ view/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Motors</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/phpmotors/css/style.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+        </header>
+        <nav>
+            <?php //require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php'; 
+            echo $navList; ?>
+        </nav>
+        <main>
+            <form class="login">
+                <h2>Log In</h2>
+                <input type="text" placeholder="First Name" required>
+                <input type="text" placeholder="Last Name" required>
+                <input type="text" placeholder="Email Address" required>
+                <input type="password" placeholder="Password" required>
+                <button>Create Account</button>
+            </form>
+        </main>
+        <hr>
+        <footer>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
