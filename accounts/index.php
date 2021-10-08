320f47cc13fb04a7c52906446b249a0ac12d573d
ZachRochette <zachrochette@hotmail.com>
2021-10-07 21:39:42 -0600

    commit

--- accounts/index.php
+++ accounts/index.php
@@ -30,11 +30,7 @@ switch ($action) {
         include 'view/home.php';
 }
 
-$clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
-if ($clientFirstname == NULL) {
-    $clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
-}
-
-switch ($clientFirstname) {
-    case ''
-}
+// $clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
+// if ($clientFirstname == NULL) {
+//     $clientFirstname = filter_input(INPUT_POST, 'clientFirstname');
+// }
