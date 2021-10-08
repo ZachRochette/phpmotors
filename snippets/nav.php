d85d1db19547a247a4c830a5fdf8a2fd7af88056
ZachRochette <zachrochette@hotmail.com>
2021-10-08 00:59:34 -0600

    commit

--- snippets/nav.php
+++ snippets/nav.php
@@ -1,5 +1,5 @@
 <ul>
-    <li> <a href="accounts?action=home" title="PHP Motors home page">Home</a></li>
+    <li> <a href="/phpmotors/accounts?action=home" title="PHP Motors home page">Home</a></li>
     <li> <a href="#" title="classic car page">Classic</a></li>
     <li> <a href="#" title="sports cars">Sports</a></li>
     <li> <a href="#" title="sports utility vehicles">SUV</a></li>
