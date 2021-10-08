5b2128fe83aa71af09e54e337d1eb4070f4df5c1
ZachRochette <zachrochette@hotmail.com>
2021-10-07 18:11:01 -0600

    commit

---
+++ view/lgogin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Motors</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/phpmotors/css/style.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+        </header>
+        <nav>
+            <?php //require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php'; 
+            echo $navList; ?>
+        </nav>
+        <main>
+            <form action="/action_page.php">
+                <label for="fname">First name:</label><br>
+                <input type="text" id="fname" name="fname" value="John"><br>
+                <label for="lname">Last name:</label><br>
+                <input type="text" id="lname" name="lname" value="Doe"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </main>
+        <hr>
+        <footer>
+            <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
