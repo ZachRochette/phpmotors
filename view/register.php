6fcaaec155965b6854d1fa319036d0a655fe5b72
ZachRochette <zachrochette@hotmail.com>
2021-10-07 21:26:32 -0600

    commit

--- view/register.php
+++ view/register.php
@@ -20,10 +20,10 @@
         <main>
             <form class="register">
                 <h2>Log In</h2>
-                <input type="text" placeholder="First Name" required>
-                <input type="text" placeholder="Last Name" required>
-                <input type="text" placeholder="Email Address" required>
-                <input type="password" placeholder="Password" required>
+                <input name="clientFirstName" id="clientFirstName" type="text" placeholder="First Name" required>
+                <input name="clientLastName" id="clientLastName" type="text" placeholder="Last Name" required>
+                <input name="clientEmail" id="clientEmail" type="text" placeholder="Email Address" required>
+                <input name="clientPassword" id="clientPassword" type="password" placeholder="Password">
                 <button>Create Account</button>
             </form>
         </main>
