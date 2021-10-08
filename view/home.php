5b9886a8343f9167c43a4434cd4e8060a51d46d7
ZachRochette <zachrochette@hotmail.com>
2021-10-07 18:32:23 -0600

    commit

--- view/home.php
+++ view/home.php
@@ -14,8 +14,7 @@
             <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
         </header>
         <nav>
-            <?php //require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php';
-            echo $navList; ?>
+            <?php echo $navList; ?>
         </nav>
         <main>
             <h1>Welcome to PHP Motors!</h1>
