655cfb7cfa1a76b1fd91808141fe5b99052de303
ZachRochette <zachrochette@hotmail.com>
2021-10-07 22:57:18 -0600

    commit

--- snippets/header.php
+++ snippets/header.php
@@ -1,4 +1,4 @@
 <div id="top-header">
-    <img src="../images/site/logo.png" alt="PHP Motors logo" id="logo">
+    <img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo">
     <a href="/phpmotors/accounts?action=login" title="Login or Register with PHP Motors" id="acc">My Account</a>
 </div>
