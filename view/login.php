bead8d8b375847ac8e35028537ddd3e83137878c
ZachRochette <zachrochette@hotmail.com>
2021-10-07 18:20:13 -0600

    commit

--- view/login.php
+++ view/login.php
@@ -18,13 +18,25 @@
             echo $navList; ?>
         </nav>
         <main>
-            <form action="/action_page.php">
+            <form class="login-form" action="javascript:void(0);">
+                <h1>Login</h1>
+                <div class="form-input-material">
+                    <input type="text" name="username" id="username" placeholder=" " autocomplete="off" class="form-control-material" required />
+                    <label for="username">Username</label>
+                </div>
+                <div class="form-input-material">
+                    <input type="password" name="password" id="password" placeholder=" " autocomplete="off" class="form-control-material" required />
+                    <label for="password">Password</label>
+                </div>
+                <button type="submit" class="btn btn-primary btn-ghost">Login</button>
+            </form>
+            <!-- <form class="login" action="/login-page.php">
                 <label for="fname">First name:</label><br>
                 <input type="text" id="fname" name="fname" value="John"><br>
                 <label for="lname">Last name:</label><br>
                 <input type="text" id="lname" name="lname" value="Doe"><br><br>
                 <input type="submit" value="Submit">
-            </form>
+            </form> -->
         </main>
         <hr>
         <footer>
