db65d1d1133dc81d000bc5cd85357707d2190f96
ZachRochette <zachrochette@hotmail.com>
2021-10-22 16:05:41 -0600

    commit

--- vehicles/index.php
+++ vehicles/index.php
@@ -19,6 +19,7 @@ foreach ($classifications as $classification) {
 }
 $navList .= '</ul>';
 
+
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_POST, 'action');
