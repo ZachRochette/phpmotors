3501302370a0bf147f847b5e65f73589e2ef98bb
ZachRochette <zachrochette@hotmail.com>
2021-10-22 16:47:32 -0600

    cmmit

--- vehicles/index.php
+++ vehicles/index.php
@@ -62,11 +62,11 @@ switch ($action) {
         $classificationId = filter_input(INPUT_POST, 'classificationId');
 
         // Check for missing data
-        if (empty($invMake) || empty($invModel) || empty($invDescription) || ($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)) {
-            $message = '<p>Please fill in all fields.</p>';
-            include '../view/add-vehicle.php';
-            exit;
-        }
+        // if (empty($invMake) || empty($invModel) || empty($invDescription) || ($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)) {
+        //     $message = '<p>Please fill in all fields.</p>';
+        //     include '../view/add-vehicle.php';
+        //     exit;
+        // }
 
         // Send the data to the inventory
         $inventory_output = input_inventory($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId);
