ee4481f1c6b2bb18898bd3c9220360d09ad8208f
ZachRochette <zachrochette@hotmail.com>
2021-10-22 03:17:09 -0600

    commit

--- vehicles/index.php
+++ vehicles/index.php
@@ -19,13 +19,6 @@ foreach ($classifications as $classification) {
 }
 $navList .= '</ul>';
 
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
-
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_POST, 'action');
