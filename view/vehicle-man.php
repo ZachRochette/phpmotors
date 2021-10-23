823259a92c29cc4c921c536f4447b968e26c6f08
ZachRochette <zachrochette@hotmail.com>
2021-10-23 02:25:47 -0600

    commit

--- view/vehicle-man.php
+++ view/vehicle-man.php
@@ -18,6 +18,7 @@
         </nav>
         <main>
             <a href="/phpmotors/vehicles?action=add-classification" title="Add car classification">Car Classification</a>
+            <br>
             <a href="/phpmotors/vehicles?action=add-vehicle" title="Add car classification">Car Inventory</a>
         </main>
         <hr>
