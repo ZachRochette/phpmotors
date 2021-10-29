5cd7914f9f60617976a4b1b2a783d181b4f70d20
ZachRochette <zachrochette@hotmail.com>
2021-10-29 16:01:34 -0600

    commit

--- view/registration.php
+++ view/registration.php
@@ -25,12 +25,18 @@
             ?>
             <form method="post" action="/phpmotors/accounts/index.php" class="register">
                 <h2>Log In</h2>
-                <label id="a" for="clientFirstname">First Name:</label>
-                <input name="clientFirstname" id="fname" type="text" placeholder="First Name" required>
+                <label for="clientFirstname">First Name:</label>
+                <input name="clientFirstname" id="clientFirstname" type="text" placeholder="First Name" <?php if (isset($clientFirstname)) {
+                                                                                                            echo "value='$clientFirstname'";
+                                                                                                        }  ?> required>
                 <label id="b" for="clientLastname">Last Name:</label>
-                <input name="clientLastname" id="lname" type="text" placeholder="Last Name" required>
+                <input name="clientLastname" id="lname" type="text" placeholder="Last Name" <?php if (isset($clientLastname)) {
+                                                                                                echo "value='$clientLastname'";
+                                                                                            }  ?>required>
                 <label id="c" for="clientEmail">Email:</label>
-                <input name="clientEmail" id="email" type="email" placeholder="Email Address" required>
+                <input name="clientEmail" id="email" type="email" placeholder="Email Address" <?php if (isset($clientEmail)) {
+                                                                                                    echo "value='$clientEmail'";
+                                                                                                }  ?>required>
                 <label for="clientPassword">Password:</label>
                 <span>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span>
                 <input type="password" name="clientPassword" id="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
