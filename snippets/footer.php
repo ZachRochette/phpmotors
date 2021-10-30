58b4b81bc2fd491cc4a05d036e635761bbf482f8
ZachRochette <zachrochette@hotmail.com>
2021-10-29 22:22:41 -0600

    commit

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,3 +1,3 @@
 <p>&copy; PHP Motors, All rights reserved.</p>
-<p>Images are belieaaved to be "Fair Use". Notify the author if not and they will be removed.</p>
+<p>Images are belieaved to be "Fair Use". Notify the author if not and they will be removed.</p>
 <p>Last Updated: <?php echo date('j F, Y', getlastmod()); ?></p>
