ac0fb81615702e170abbf679dd80395c79b6a217
ZachRochette <zachrochette@hotmail.com>
2021-10-29 22:21:54 -0600

    commit

--- view/add-vehicle.php
+++ view/add-vehicle.php
@@ -1,3 +1,17 @@
+<?php
+$classifList = '<select name="classificationId">';
+foreach ($classifications as $classification) {
+    $classifList .= "<option value='$classification[classificationId]'";
+    if (isset($classificationId)) {
+        if ($classifiaction['classificationID'] === $classificationId) {
+            $classifList .= ' selected ';
+        }
+    }
+
+    $classifList .= ">$classification[classificationName]</option>";
+}
+$classifList .= '</select>';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
