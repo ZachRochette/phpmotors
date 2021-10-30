b2d05df9fc90f5a556ee66f717fd47b3f537fe99
ZachRochette <zachrochette@hotmail.com>
2021-10-29 22:45:37 -0600

    commit

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ require_once 'model/main-model.php';
 
 // Get the array of classifications
 $classifications = getClassifications();
-//Build the navigation bar
-$navList = buildNavigation($classifications);
+//build navigation
+$navList = Navigation($classifications);
+
+// // Build a navigation bar using the $classifications array
+// $navList = '<ul>';
+// $navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+// foreach ($classifications as $classification) {
+//     $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+// }
+// $navList .= '</ul>';
 
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
