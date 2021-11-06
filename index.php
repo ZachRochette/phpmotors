1c65b4f39f1d9c06ec65eb3e7eeac48c04270152
ZachRochette <zachrochette@hotmail.com>
2021-11-06 03:12:37 -0600

    commit

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ session_start();
 require_once 'library/connections.php';
 // Get the PHP Motors model for use as needed
 require_once 'model/main-model.php';
-
+// Get the functions library
+require_once 'library/functions.php';
 
 // Check if the firstname cookie exists, get its value
 if (isset($_COOKIE['firstname'])) {
@@ -17,15 +18,15 @@ if (isset($_COOKIE['firstname'])) {
 // Get the array of classifications
 $classifications = getClassifications();
 //build navigation
-// $navList = Navigation($classifications);
+$navList = Navigation($classifications);
 
 // // Build a navigation bar using the $classifications array
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
+// $navList = '<ul>';
+// $navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+// foreach ($classifications as $classification) {
+//     $navList .= "<li><a href='/phpmotors/index.php?action=" . urlencode($classification['classificationName']) . "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+// }
+// $navList .= '</ul>';
 
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
