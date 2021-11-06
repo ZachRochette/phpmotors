f795409101700b90631309e9adb6e276c05d44b6
ZachRochette <zachrochette@hotmail.com>
2021-11-06 02:33:09 -0600

    Revert "commit"
    This reverts commit 2e2dcaf376dd8a01a1ce8af1b3c4c17715a46f37.

--- accounts/index.php
+++ accounts/index.php
@@ -1,10 +1,6 @@
 <?php
 // Accounts Controller
 
-// Create or access a Session
-session_start();
-
-
 // Get the database connection file
 require_once '../library/connections.php';
 // Get the PHP Motors model for use as needed
@@ -44,14 +40,6 @@ switch ($action) {
         $clientPassword = trim(filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING));
         $clientEmail = checkEmail($clientEmail);
         $checkPassword = checkPassword($clientPassword);
-        //Check for existing email address
-        $existingEmail = checkExistingEmail($clientEmail);
-
-        if ($existingEmail) {
-            $message = '<p class="notice">That email address already exists. Do you want to login instead?</p>';
-            include '../view/login.php';
-            exit;
-        }
 
         // Check for missing data
         if (empty($clientFirstname) || empty($clientLastname) || empty($clientEmail) || empty($checkPassword)) {
@@ -68,7 +56,6 @@ switch ($action) {
 
         // Check and report the result
         if ($regOutcome === 1) {
-            setcookie('firstname', $clientFirstname, strtotime('+1 year'), '/');
             $message = "<p>Thanks for registering $clientFirstname. Please use your email and password to login.</p>";
             include '../view/login.php';
             exit;
--- snippets/header.php
+++ snippets/header.php
@@ -1,7 +1,4 @@
 <div id="top-header">
     <img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo">
-    <?php if (isset($cookieFirstname)) {
-        echo "<span>Welcome $cookieFirstname</span>";
-    } ?>
     <a href="/phpmotors/accounts?action=login" title="Login or Register with PHP Motors" id="acc">My Account</a>
 </div>
