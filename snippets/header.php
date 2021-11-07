872557602b3224a6bf061ea2d77a73ba8bcd197a
ZachRochette <zachrochette@hotmail.com>
2021-11-06 21:44:18 -0600

    Enhancement5

--- snippets/header.php
+++ snippets/header.php
@@ -1,9 +1,9 @@
 <div id="top-header">
     <img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo">
     <?php if ($_SESSION['loggedin']) {
-        echo "<a href='/phpmotors/accounts/index.php?action=" . urlencode('admin') . "' title='Admin Page' id='acc'>Welcome ", $_SESSION['clientData']['clientFirstname'], ". $logout</a>";
+        echo "<a href='/phpmotors/accounts/index.php?action=" . urlencode('admin') . "' title='Admin Page' class='acc'>Welcome ", $_SESSION['clientData']['clientFirstname'], ". $logout</a>";
     } else {
-        echo "<a href='/phpmotors/accounts?action=login' title='Login or Register with PHP Motors' id='acc'>My Account</a>";
+        echo "<a href='/phpmotors/accounts?action=login' title='Login or Register with PHP Motors' class='acc'>My Account</a>";
     } ?>
     <!-- <a href="/phpmotors/accounts?action=login" title="Login or Register with PHP Motors" id="acc">My Account</a> -->
 </div>
